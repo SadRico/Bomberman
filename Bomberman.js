95db359a48427213a61b4aa96df5503ad00f1565
SadRico <134720995+SadRico@users.noreply.github.com>
2025-02-06 11:02:53 +0100

    Add files via upload

--- Bomberman.js
+++ Bomberman.js
@@ -319,24 +319,33 @@ class Explosion extends Substance {
 
 const playerImages = {
     idle: new Image(),
-    walkDown: [new Image(), new Image()],
-    walkUp: [new Image(), new Image()],
-    walkRight: [new Image(), new Image()],
-    walkLeft: [new Image(), new Image()]
+    walkDown: [new Image(), new Image(), new Image(), new Image()],
+    walkUp: [new Image(), new Image(), new Image(), new Image()],
+    walkRight: [new Image(), new Image(), new Image(), new Image()],
+    walkLeft: [new Image(), new Image(), new Image(), new Image()]
 };
 
 playerImages.idle.src = 'assets/bomberman_idle.png';
 playerImages.walkDown[0].src = 'assets/bomberman_walk_down_1.png';
-playerImages.walkDown[1].src = 'assets/bomberman_walk_down_2.png';
+playerImages.walkDown[1].src = 'assets/bomberman_idle.png';
+playerImages.walkDown[2].src = 'assets/bomberman_walk_down_2.png';
+playerImages.walkDown[3].src = 'assets/bomberman_idle.png';
+
 
 playerImages.walkUp[0].src = 'assets/bomberman_walk_up_1.png';
-playerImages.walkUp[1].src = 'assets/bomberman_walk_up_2.png';
+playerImages.walkUp[1].src = 'assets/bomberman_walk_up_idle.png';
+playerImages.walkUp[2].src = 'assets/bomberman_walk_up_2.png';
+playerImages.walkUp[3].src = 'assets/bomberman_walk_up_idle.png';
 
 playerImages.walkRight[0].src = 'assets/bomberman_walk_right_1.png';
-playerImages.walkRight[1].src = 'assets/bomberman_walk_right_2.png';
+playerImages.walkRight[1].src = 'assets/bomberman_walk_right_idle.png';
+playerImages.walkRight[2].src = 'assets/bomberman_walk_right_2.png';
+playerImages.walkRight[3].src = 'assets/bomberman_walk_right_idle.png';
 
 playerImages.walkLeft[0].src = 'assets/bomberman_walk_left_1.png';
-playerImages.walkLeft[1].src = 'assets/bomberman_walk_left_2.png';
+playerImages.walkLeft[1].src = 'assets/bomberman_walk_left_idle.png';
+playerImages.walkLeft[2].src = 'assets/bomberman_walk_left_2.png';
+playerImages.walkLeft[3].src = 'assets/bomberman_walk_left_idle.png';
 
 
 
@@ -401,16 +410,16 @@ class Player {
 
         // Update Position und Bildwechsel für Animation
         if (this.isMovingDown) {
-            this.walkingDownFrame = (this.walkingDownFrame + 1) % 2; // Wechselt zwischen den beiden Bildern
+            this.walkingDownFrame = (this.walkingDownFrame + 1) % 4; // Wechselt zwischen den beiden Bildern
         }
         if (this.isMovingUp) {
-            this.walkingUpFrame = (this.walkingUpFrame + 1) % 2;
+            this.walkingUpFrame = (this.walkingUpFrame + 1) % 4;
         }
         if (this.isMovingRight) {
-            this.walkingRightFrame = (this.walkingRightFrame + 1) % 2;
+            this.walkingRightFrame = (this.walkingRightFrame + 1) % 4;
         }
         if (this.isMovingLeft) {
-            this.walkingLeftFrame = (this.walkingLeftFrame + 1) % 2;
+            this.walkingLeftFrame = (this.walkingLeftFrame + 1) % 4;
         }
 
 
